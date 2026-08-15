b15a44ac1e17dc6baf371d85e18a778d58a788de
G.Cuong <legiacuong789@gmail.com>
2026-08-15 17:20:37 +0700

    update: increase the timeout to 60s for testing

--- frontend/src/configs/apis/index.ts
+++ frontend/src/configs/apis/index.ts
@@ -4,7 +4,7 @@ import paths from "../constants/paths";
 
 const apiClient = axios.create({
 	baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
-	timeout: 30000,
+	timeout: 60000,
 	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
